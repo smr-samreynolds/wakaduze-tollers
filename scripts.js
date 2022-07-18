79f1ffad030b92c0bc332a61ecaf2f8e5657dde1
smr-samreynolds <94646244+smr-samreynolds@users.noreply.github.com>
2022-07-18 13:58:51 +0100

    Homepage complete

--- scripts.js
+++ scripts.js
@@ -2,3 +2,8 @@ $('.hamburger-button').click(function(){
     $('.mobile-menu').fadeToggle(100);
     $(this).toggleClass('active');
 });
+
+lightGallery(document.getElementById('lightgallery'), {
+    speed: 500,
+    download: false
+});
